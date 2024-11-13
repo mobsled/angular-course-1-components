9dc77c8b6c987bf3a724347d945572d91b59ccd5
martingajdos <martin.gajdos@uniqa.at>
2024-11-13 16:44:23 +0100

    test commit

--- src/app/my-card/my-card.component.ts
+++ src/app/my-card/my-card.component.ts
@@ -9,26 +9,15 @@ import { Course } from '../model/course';
   styleUrl: './my-card.component.css'
 })
 export class MyCardComponent {
-  @Input({ 
-    required: true 
+  @Input({
+    required: true
   })
   course: Course;
 
-  @Input({ 
-    required: false 
+  @Input({
+    required: false
   })
   index: Number;
-
-  // Dobrý deň, tu je Gajdoš pri telefóne.
-  // Ja by som mal mat dneska preventivku 10:50.
-  
-  // Viete co volam kvoli tomu, ja sa velmi ospravedlnujem, 
-  // musel som niečo chytiť v robote tento týždeň, meral som si rano teplotu 
-  // a mal som 38. Ja by som vam bol byval zavolal aj skor, no vcera som bol este uplne v poriadku.
-  
-  // Chcel by som sa prosím spýtať, či by som sa nemohol s vami dohodnúť
-  // možno na náhradnom termíne ak by to bolo možné. Dosť ľudí je aj v robote chorých teraz.
-  // Behnem dnes lekárovi.
 
   @Input()
   cardIndex: Number;
